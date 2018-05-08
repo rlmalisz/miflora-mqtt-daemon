e0a1f328f727f7294a44285248bbd6b32c43e8d7
[Richard Maliszewski] <[rlmalisz@nguzu.com]>
2018-05-07 17:42:44 -0700

    [changed errors expected from blewrap]

--- miflora-mqtt-daemon.py
+++ miflora-mqtt-daemon.py
@@ -14,6 +14,7 @@ from configparser import ConfigParser
 from unidecode import unidecode
 from miflora.miflora_poller import MiFloraPoller, MI_BATTERY, MI_CONDUCTIVITY, MI_LIGHT, MI_MOISTURE, MI_TEMPERATURE
 from btlewrap import available_backends, BluepyBackend, GatttoolBackend, PygattBackend
+from btlewrap.base import BluetoothBackendException
 import paho.mqtt.client as mqtt
 import sdnotify
 
@@ -218,7 +219,7 @@ for [name, mac] in config['Sensors'].items():
         flora_poller.fill_cache()
         flora_poller.parameter_value(MI_LIGHT)
         flora['firmware'] = flora_poller.firmware_version()
-    except IOError:
+    except (IOError,BluetoothBackendException):
         print_line('Initial connection to Mi Flora sensor "{}" ({}) failed.'.format(name_pretty, mac), error=True, sd_notify=True)
     else:
         print('Internal name: "{}"'.format(name_clean))
@@ -313,7 +314,7 @@ while True:
             try:
                 flora['poller'].fill_cache()
                 flora['poller'].parameter_value(MI_LIGHT)
-            except IOError:
+            except (IOError,BluetoothBackendException):
                 attempts = attempts - 1
                 if attempts > 0:
                     print_line('Retrying ...', warning = True)
